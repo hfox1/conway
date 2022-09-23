2cf744ab14d7cc128ef04c39551e1fbb3eb40c1e
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-09-23 20:55:47 +0100

    neighbourhood tests broke when rewriting method/class names

--- neighbourhood.js
+++ neighbourhood.js
@@ -1,46 +1,48 @@
 class NeighbourhoodConstructor {
   constructor(board, i, j) {
-    this.board = board
-    this.boardWidth = board[0].length
-    this.boardHeight = board.length
-    this.i = i
-    this.j = j
-    this.neighbourhood = 'empty initialised neighbourhood'
+    this.board = board;
+    this.boardWidth = board[0].length;
+    this.boardHeight = board.length;
+    this.i = i;
+    this.j = j;
+    this.neighbourhood = 'empty initialised neighbourhood';
   }
 
   neighbourhood() {
-    var i = this.i
-    var j = this.j
-    let populatedNeighbourhood = [[], [], []]
+    var i = this.i;
+    var j = this.j;
+    let populatedNeighbourhood = [[], [], []];
 
     for (let p = 0; p < 3; p++) {
       if (!this.board[j - 1 + p]) {
         //if the row doesnt exist:
-        populatedNeighbourhood[p] = [0, 0, 0] // then neighbourhood row is 000
+        populatedNeighbourhood[p] = [0, 0, 0]; // then neighbourhood row is 000
       } else {
         // if the row exists
         for (let q = 0; q < 3; q++) {
           //look along the three columns
           if (!this.board[j - 1 + p][i - 1 + q]) {
             //if the column doesnt exist
-            populatedNeighbourhood[p].push(0)
+            populatedNeighbourhood[p].push(0);
           } else {
-            populatedNeighbourhood[p].push(this.board[j - 1 + p][i - 1 + q]) // push in the neighbour
+            populatedNeighbourhood[p].push(this.board[j - 1 + p][i - 1 + q]); // push in the neighbour
           }
         }
       }
     }
 
-    populatedNeighbourhood[1][1] = 0 //delete target square
-    this.neighbourhood = populatedNeighbourhood
-    return populatedNeighbourhood
+    populatedNeighbourhood[1][1] = 0; //delete target square
+    this.neighbourhood = populatedNeighbourhood;
+    return populatedNeighbourhood;
   }
 
   score() {
     if (this.neighbourhood === 'empty initialised neighbourhood') {
-      return 'tried to score an empty neighbourhood'
+      throw new Error('tried to score an empty initialised neighbourhood');
     }
+    const score = this.neighbourhood.reduce((a, b) => a + b, 0); //probably needs to be flatmap.reduce
+    return score;
   }
 }
 
-module.exports = NeighbourhoodConstructor
+module.exports = NeighbourhoodConstructor;
--- spec/neighbourhood.spec.js
+++ spec/neighbourhood.spec.js
@@ -1,6 +1,6 @@
 // 'Neighbours' off the edge of the board are not functioning neighbours
 // A square has a neighbourhood score
-const NeighbourhoodConstructor = require('../neighbourhood')
+const NeighbourhoodConstructor = require('../neighbourhood');
 
 describe('NeighbourhoodConstructor', () => {
   const numBoard = [
@@ -10,7 +10,7 @@ describe('NeighbourhoodConstructor', () => {
     [0, 0, 0, 0, 0],
     [1, 1, 1, 1, 1],
     // i
-  ]
+  ];
 
   const letterBoard = [
     ['a', 'b', 'c', 'd', 'e'], // i
@@ -19,41 +19,41 @@ describe('NeighbourhoodConstructor', () => {
     ['p', 'q', 'r', 's', 't'],
     ['u', 'v', 'w', 'y', 'x'],
     // j
-  ]
+  ];
 
   it('returns a neighbourhood (from middle of board)', () => {
-    let neighbourhoodConstructor = new NeighbourhoodConstructor(letterBoard, 3, 2) // origin square "n"
+    let neighbourhoodConstructor = new NeighbourhoodConstructor(letterBoard, 3, 2); // origin square "n"
     // expect:
     // hij
     // m0o
     // rst
-    let neighbours = neighbourhoodConstructor.neighbourhood()
-    expect(neighbours[0][0]).toBe('h')
-    expect(neighbours[0][1]).toBe('i')
-    expect(neighbours[0][2]).toBe('j')
-    expect(neighbours[1][0]).toBe('m')
-    expect(neighbours[1][1]).toBe(0)
-    expect(neighbours[1][2]).toBe('o')
-    expect(neighbours[2][0]).toBe('r')
-    expect(neighbours[2][1]).toBe('s')
-    expect(neighbours[2][2]).toBe('t')
-  })
+    let neighbours = neighbourhoodConstructor.neighbourhood();
+    expect(neighbours[0][0]).toBe('h');
+    expect(neighbours[0][1]).toBe('i');
+    expect(neighbours[0][2]).toBe('j');
+    expect(neighbours[1][0]).toBe('m');
+    expect(neighbours[1][1]).toBe(0);
+    expect(neighbours[1][2]).toBe('o');
+    expect(neighbours[2][0]).toBe('r');
+    expect(neighbours[2][1]).toBe('s');
+    expect(neighbours[2][2]).toBe('t');
+  });
 
   it('set neighbours off the edge of the board to 0', () => {
     // expect:
     // 000
     // a0c
     // fgh
-    let neighbourhoodConstructor = new NeighbourhoodConstructor(letterBoard, 1, 0) // origin square is "b"
-    let neighbours = neighbourhoodConstructor.neighbourhood()
-    expect(neighbours[0][0]).toBe(0)
-    expect(neighbours[0][1]).toBe(0)
-    expect(neighbours[0][2]).toBe(0)
-    expect(neighbours[1][0]).toBe('a')
-    expect(neighbours[1][1]).toBe(0)
-    expect(neighbours[1][2]).toBe('c')
-    expect(neighbours[2][0]).toBe('f')
-    expect(neighbours[2][1]).toBe('g')
-    expect(neighbours[2][2]).toBe('h')
-  })
-})
+    let neighbourhoodConstructor = new NeighbourhoodConstructor(letterBoard, 1, 0); // origin square is "b"
+    let neighbours = neighbourhoodConstructor.neighbourhood();
+    expect(neighbours[0][0]).toBe(0);
+    expect(neighbours[0][1]).toBe(0);
+    expect(neighbours[0][2]).toBe(0);
+    expect(neighbours[1][0]).toBe('a');
+    expect(neighbours[1][1]).toBe(0);
+    expect(neighbours[1][2]).toBe('c');
+    expect(neighbours[2][0]).toBe('f');
+    expect(neighbours[2][1]).toBe('g');
+    expect(neighbours[2][2]).toBe('h');
+  });
+});
